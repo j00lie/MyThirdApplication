f70beb8d3ab944ca58db8fef02d6b22482f512ca
j00lie <joonas.liedes@student.lut.fi>
2024-08-28 14:45:38 +0300

    Finished task 3

--- app/src/main/java/com/example/mythirdapplication/DetailActivity.kt
+++ app/src/main/java/com/example/mythirdapplication/DetailActivity.kt
@@ -1,6 +1,11 @@
 package com.example.mythirdapplication
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.Image
 import android.os.Bundle
+import android.view.Display
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,43 @@ class DetailActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val index: Int = intent.getIntExtra("com.example.mythirdapplication.ITEM_INDEX", -1)
+
+        if (index > -1){
+            val pic = getImg(index)
+            val img: ImageView = findViewById(R.id.imageView)
+            scaleImg(img, pic)
+        }
+
+    }
+
+    private fun getImg(index: Int): Int{
+        return when (index){
+            0 -> R.drawable.peach
+            1 -> R.drawable.tomato
+            2 -> R.drawable.squash
+            else -> -1
+
+        }
+    }
+    private fun scaleImg(img: ImageView, pic: Int){
+        val screen: Display = windowManager.defaultDisplay
+        val options = BitmapFactory.Options()
+
+        options.inJustDecodeBounds = true
+        BitmapFactory.decodeResource(resources, pic, options)
+
+        val imgWidth: Int = options.outWidth
+        val screenWidth: Int = screen.width
+
+        if (imgWidth > screenWidth){
+            val ratio = Math.round((imgWidth / screenWidth).toDouble()).toInt()
+            options.inSampleSize = ratio
+        }
+
+        options.inJustDecodeBounds = false
+        val scaledImg = BitmapFactory.decodeResource(resources, pic, options)
+        img.setImageBitmap(scaledImg)
     }
 }
